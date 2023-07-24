cd9221c92ee30fa20bd157ed75b71c6eb5414d69
apete <apete@optimatika.se>
2023-07-24 16:29:57 +0200

    Remove commercial solvers

--- src/test/java/org/ojalgo/benchmark/linear/netlib/NetlibBenchmark.java
+++ src/test/java/org/ojalgo/benchmark/linear/netlib/NetlibBenchmark.java
@@ -38,8 +38,7 @@ public final class NetlibBenchmark extends AbstractBenchmark {
     static final int MAX_NB_VARS = 1000;
     static final int MIN_NB_VARS = 1;
 
-    static final String[] SOLVERS = { Contender.OJALGO_NEW_TABLEAU_LP, Contender.OJALGO_NEW_REVISED_LP, Contender.OJALGO_OLD_DENSE_LP, Contender.ORTOOLS,
-            Contender.CPLEX };
+    static final String[] SOLVERS = { Contender.OJALGO_NEW_TABLEAU_LP, Contender.OJALGO_NEW_REVISED_LP, Contender.OJALGO_OLD_DENSE_LP, Contender.ORTOOLS };
     // static final String[] SOLVERS = { Contender.OJALGO_NEW_REVISED_LP };
     static final Set<ModelSolverPair> WORK = new HashSet<>();
 
@@ -81,7 +80,7 @@ public final class NetlibBenchmark extends AbstractBenchmark {
 
         configuration.pathPrefix = "/optimisation/netlib/";
 
-        configuration.refeenceSolver = Contender.CPLEX;
+        configuration.refeenceSolver = Contender.ORTOOLS;
 
         AbstractBenchmark.doBenchmark(WORK, configuration);
     }
